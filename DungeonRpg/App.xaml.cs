004d2680f4689c9519062b5bfdccd4396429b9fb
Bishi77 <john.csima@gmail.com>
2021-04-27 23:21:39 +0200

    + error eventhandlers

--- DungeonRpg/App.xaml.cs
+++ DungeonRpg/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
 
 namespace DungeonRpg.Views
 {
@@ -7,5 +9,21 @@ namespace DungeonRpg.Views
 	/// </summary>
 	public partial class App : Application
 	{
+		public App()
+		{
+			this.Dispatcher.UnhandledException += OnDispatcherUnhandledException;			
+			Application.Current.DispatcherUnhandledException += OnDispatcherUnhandledException;
+			//AppDomain.CurrentDomain.UnhandledException += ...;
+			//TaskScheduler.UnobservedTaskException += ...;
+		}
+
+		void OnDispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+		{
+			string errorMessage = string.Format("An unhandled exception occurred: {0}", e.Exception.Message);
+			MessageBox.Show(errorMessage, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+			// OR whatever you want like logging etc. MessageBox it's just example
+			// for quick debugging etc.
+			e.Handled = true;
+		}
 	}
 }
