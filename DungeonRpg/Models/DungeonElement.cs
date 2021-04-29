65b7eaf69006fdfd2df0cfeea812d38fa8cb8b05
Bishi77 <john.csima@gmail.com>
2021-04-30 00:04:38 +0200

    komment kiszedés

--- DungeonRpg/Models/DungeonElement.cs
+++ DungeonRpg/Models/DungeonElement.cs
@@ -5,8 +5,6 @@
 	/// </summary>
 	public enum DungeonElementType { Wall = 0, StartPoint = 1, EndPoint = 2, Item = 3, Player = 4, Monster = 5, UpStairs = 6, DownStairs = 7, Way = 8 }
 	
-	//régi mezők, megfeleltetni az újjal
-	//public enum FieldTypes { Wall = 0, Finish = 1, Down = 2, Up = 3, Monster = 4, Way = 5, Start = 6 };
 	public class DungeonElement
 	{
 		/// <summary>
