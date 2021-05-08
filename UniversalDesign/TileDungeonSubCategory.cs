3532c05aa728f406b00aabf874167041414d605a
Bishi77 <john.csima@gmail.com>
2021-05-08 07:30:03 +0200

    kódszépítés, Viewk átnevezése, felesleges usingok kivétele

--- UniversalDesign/TileDungeonSubCategory.cs
+++ UniversalDesign/TileDungeonSubCategory.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UniversalDesign
+﻿namespace UniversalDesign
 {
-    public class TileDungeonSubCategory : TileSubCategory
+	public class TileDungeonSubCategory : TileSubCategory
     {
         private TileDungeonSubCategory(string value) : base(value) {}
 
