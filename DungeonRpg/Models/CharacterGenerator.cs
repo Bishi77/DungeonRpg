078ccbeb9f4c4a346864780e829dc2f18d585957
Bishi77 <john.csima@gmail.com>
2021-05-10 19:23:35 +0200

    Karakter név generálása, csata kép igazítás

--- DungeonRpg/Models/CharacterGenerator.cs
+++ DungeonRpg/Models/CharacterGenerator.cs
@@ -13,9 +13,77 @@ namespace DungeonRpg.Models
 		public static Player GeneratePlayer(ValueTuple<int, int> startPosition, Dungeon dungeon, int diceSize, int diceNr)
 		{
 			Player player = new Player(diceSize, diceNr, startPosition);
+			GenerateName(player);
 			dungeon.AddDungeonElementByPosition(player.Position.Item1, player.Position.Item2, new DungeonElement(DungeonElementType.Player), true);
 			player.Inventory.ItemList = InventoryItemGenerator.GenerateRandomItems(10);
 			return player;
+		}
+
+		private static void GenerateName(Player player)
+		{
+			string characterName;
+			List<string> nameParts = new List<string>();
+			bool isHigh;
+			Dictionary<string, int> attribs = new Dictionary<string, int>();
+			attribs.Add(nameof(player.Strength), player.Strength);
+			attribs.Add(nameof(player.Wisdom), player.Wisdom);
+			attribs.Add(nameof(player.Intelligence), player.Intelligence);
+			attribs.Add(nameof(player.Dexterity), player.Dexterity);
+			attribs.Add(nameof(player.Constitution), player.Constitution);
+			attribs.Add(nameof(player.Charisma), player.Charisma);
+
+			var minKey = attribs.Aggregate((l, r) => l.Value < r.Value ? l : r);
+			var maxKey = attribs.Aggregate((l, r) => l.Value > r.Value ? l : r);
+			
+			isHigh = ((minKey.Value - 3) >= (18 - maxKey.Value));
+			var actKey = isHigh ? maxKey : minKey;
+
+			switch (actKey.Key)
+			{
+				case nameof(player.Strength):
+					if (isHigh)
+						nameParts = new List<string>() { "Óriás", "Big", "Pici", "Hegy", "Medveölő" };
+					else
+						nameParts = new List<string>() { "Zabszem", "Törpe", "Bányász", "Földtúró", "Gyáva" };
+					break;
+				case nameof(player.Charisma):
+					if (isHigh)
+						nameParts = new List<string>() { "Sármos", "Arany", "Long", "Szépfiú" };
+					else
+						nameParts = new List<string>() { "Durva", "Csúnya", "Sebhelyes", "Varangy", "Fogatlan" };
+					break;
+				case nameof(player.Constitution):
+					if (isHigh)
+						nameParts = new List<string>() { "Tiszta", "Szép", "Makulátlan", "Szépfiú" };
+					else
+						nameParts = new List<string>() { "Beteges", "Csúnya", "Sebhelyes", "Kérész", "Fogatlan" };
+					break;
+				case nameof(player.Dexterity):
+					if (isHigh)
+						nameParts = new List<string>() { "Ügyes", "Gyors", "Fegyvermester", "Késes" };
+					else
+						nameParts = new List<string>() { "Lyukaskezű", "Szobor", "Csiga", "Ügyetlen", "Jókiejtésű" };
+					break;
+				case nameof(player.Intelligence):
+					if (isHigh)
+						nameParts = new List<string>() { "Okos", "Ravasz", "Róka", "Számító" };
+					else
+						nameParts = new List<string>() { "Buta", "Lassú", "Értetlen", "Félfogású", "Sötét" };
+					break;
+				case nameof(player.Wisdom):
+					if (isHigh)
+						nameParts = new List<string>() { "Bölcs", "Tiszta", "Erkölcsös", "Mester", "Tanító", "Tiszta" };
+					else
+						nameParts = new List<string>() { "Mocskos", "Sötét", "Erkölcstelen", "Rabló", "Modortalan" };
+					break;
+				default:
+					nameParts = new List<string>() { "Csak", "Egyszerű" };
+					break;
+			}
+			characterName = nameParts[Dice.Rnd.Next(nameParts.Count)];
+			nameParts = new List<string>() { "Pista", "Jóska", "János", "Péter", "Béla" };
+			characterName += " " + nameParts[Dice.Rnd.Next(nameParts.Count)];
+			player.Name = characterName;
 		}
 
 		public static Monster GenerateMonster((int, int) startPosition, Dungeon dungeon, int diceSize, int diceNr)
---
+++ DungeonRpg/Models/List.cs
@@ -0,0 +1,9 @@
+﻿namespace DungeonRpg.Models
+{
+	internal class List<T1, T2>
+	{
+		public List()
+		{
+		}
+	}
+}
